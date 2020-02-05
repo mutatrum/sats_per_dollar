4a5947f100095e267597311e737107c085ac48fe
mutatrum <mutatrum@gmail.com>
2020-02-05 15:01:12 +0100

    Add reply-to function

--- sats_per_dollar.js
+++ sats_per_dollar.js
@@ -9,10 +9,16 @@ const WIDTH = 506;
 const GRID = 10;
 const COLUMNS = 16;
 
+var in_reply_to;
+
 (async function () {
   
   console.log(`started ${new Date().toISOString()}`);
-
+ 
+  if (process.argv.length > 2) {
+    in_reply_to = process.argv[2];
+  }
+  
   var result = await getPrice();
   
   var price = result[0];
@@ -26,7 +32,7 @@ const COLUMNS = 16;
   var b = Math.floor(Math.random() * 256);
   
   var background = (255 << 24) + (b << 16) + (g << 8) + r;
-  var color = (r * 0.299 + g * 0.587 + b * 0.114) > 186 ? 0xFF000000 : 0xFFFFFFFF;
+  var color = (r * 0.299 + g * 0.587 + b * 0.114) > 149 ? 0xFF000000 : 0xFFFFFFFF;
   
   var height = getHeight(sats);
 
@@ -46,12 +52,7 @@ const COLUMNS = 16;
     var x = 6 + (ax * 3) + (bx * 31);
     var y = 6 + (ay * 3) + (by * 31);
     
-    var p = (y * WIDTH) + x;
-    
-    pixels[p            ] = color;
-    pixels[p         + 1] = color;
-    pixels[p + WIDTH    ] = color;
-    pixels[p + WIDTH + 1] = color;
+    dot(pixels, x, y, color);
     
     ax++;
     if (ax == GRID) {
@@ -76,42 +77,95 @@ const COLUMNS = 16;
   const out = fs.createWriteStream('image.png');
   const stream = canvas.createPNGStream();
   stream.pipe(out);
-  out.on('finish', () =>  post(sats));
+  out.on('finish', () =>  postStatus(sats));
 })();
+
+function dot(pixels, x, y, color) {
+  var p = (y * WIDTH) + x;
+  
+  pixels[p            ] = color;
+  pixels[p         + 1] = color;
+  pixels[p + WIDTH    ] = color;
+  pixels[p + WIDTH + 1] = color;
+}
 
 function getHeight(sats) {
   var rows = Math.floor(sats / (COLUMNS * GRID * GRID)) + 1;
   return (rows * 31) + 10;
 }
 
-function post(sats) {
+async function postStatus(sats) {
   var twitter = new Twitter(config);
+  
+  if (in_reply_to) {
+    var reply = await getStatusesShow(twitter, in_reply_to);
+    screen_name = reply.user.screen_name;
+    for (var name of reply.text.split(' ')) {
+      if (name.startsWith('@') && screen_name.indexOf(name) == -1) {
+        screen_name = screen_name + ' ' + name;
+      }
+    }
+    console.log(`in reply to @${screen_name}`);
+  }
   
   const imageData = fs.readFileSync("image.png");
   
-  twitter.post("media/upload", {media: imageData}, function(error, media, response) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(`media/upload: ${response.statusCode} ${response.statusMessage}`);
-      const status = {
-        status: sats,
-        media_ids: media.media_id_string
+  var media = await postMediaUpload(twitter, imageData);
+  
+  var status = {
+    status: sats,
+    media_ids: media.media_id_string
+  }
+  
+  if (in_reply_to) {
+    status.status = `@${screen_name} ${sats}`;
+    status.in_reply_to_status_id = in_reply_to;
+  }
+  
+  var tweet = postStatusesUpdate(twitter, status)
+ 
+  console.log(`tweet id ${tweet.id}`);
+
+  fs.unlinkSync('image.png');
+  console.log(`finished ${new Date().toISOString()}`);
+}
+
+function getStatusesShow(twitter, id) {
+  return new Promise(function(resolve, reject) {
+    twitter.get("statuses/show/" + id, {}, function(error, media, response) {
+      if (error) {
+        reject(error);
+      } else {
+        console.log(`GET statuses/show: ${response.statusCode} ${response.statusMessage}`);
+        resolve(media);
       }
-   
-      twitter.post("statuses/update", status, function(error, tweet, response) {
-        if (error) {
-          console.log(error);
-        } else {
-          
-          console.log(`statuses/update: ${response.statusCode} ${response.statusMessage}`);
-          console.log(`tweet id ${tweet.id}`);
-          
-          fs.unlinkSync('image.png');
-          console.log(`finished ${new Date().toISOString()}`);
-        }
-      });
-    }
+    });    
+  });
+}
+
+function postMediaUpload(twitter, imageData) {
+  return new Promise(function(resolve, reject) {
+    twitter.post("media/upload", {media: imageData}, function(error, media, response) {
+      if (error) {
+        reject(error);
+      } else {
+        console.log(`POST media/upload: ${response.statusCode} ${response.statusMessage}`);
+        resolve(media);
+      }
+    });    
+  });
+}
+
+function postStatusesUpdate(twitter, status) {
+  return new Promise(function(resolve, reject) {
+    twitter.post("statuses/update", status, function(error, tweet, response) {
+      if (error) {
+        reject(error);
+      } else {
+        console.log(`POST statuses/update: ${response.statusCode} ${response.statusMessage}`);
+        resolve(tweet);
+      }
+    });
   });
 }
 
