ffa2e49551176e62b2d7a82c8d6ae852d79a4fbe
mutatrum <mutatrum@gmail.com>
2020-04-12 21:57:00 +0200

    Fix usernames

--- sats_per_dollar.js
+++ sats_per_dollar.js
@@ -114,8 +114,8 @@ async function postStatus(sats) {
   if (in_reply_to) {
     var reply = await getStatusesShow(twitter, in_reply_to);
     screen_name = reply.user.screen_name;
-    for (var name of reply.text.split(' ')) {
-      if (name.startsWith('@') && screen_name.indexOf(name) == -1) {
+    for (var name of reply.text.match(/@[a-zA-Z0-9_]*/g)) {
+      if (screen_name.indexOf(name) == -1) {
         screen_name = screen_name + ' ' + name;
       }
     }
