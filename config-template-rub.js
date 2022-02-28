a26068af84c5e72aaff15610cb29c580c70405e9
mutatrum <mutatrum@gmail.com>
2022-02-28 15:07:15 +0100

    Add template for ruble

---
+++ config-template-rub.js
@@ -0,0 +1,12 @@
+module.exports = {
+  screen_name: 'sats_per_ruble',
+  schedule: '0 */4 * * *',
+  uri: 'https://api.binance.com/api/v3/ticker/bookTicker?symbol=BTCRUB',
+  eval: 'result.askPrice',
+  text_bottom_left: 'We are all hodlonaut',
+  text_bottom_right: 'CSW is a fraud',
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: '',
+}
